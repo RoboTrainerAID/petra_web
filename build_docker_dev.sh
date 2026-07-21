5a67d0b96c98fb19f81f802354c3a5a9c990c021
Yuliia Cormier <yantsur@gmail.com>
2026-07-21 17:09:01 +0200

    added development docker file and build script

---
+++ build_docker_dev.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+docker build \
+  -t petra_web:dev \
+  --build-arg="BACKEND_URL=http://localhost:5000" \
+  -f Dockerfile.dev .
+
+echo "Run Container"
+xhost + local:root
+
+docker run \
+  --rm \
+  -it \
+  --name petra_web \
+  -p 3000:3000 \
+  -v "$(pwd)":/home/node/petra_web \
+  -v /home/node/petra_web/node_modules \
+  petra_web:dev
